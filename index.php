72a0f59e78baa7e2fb37dfe729cfefb61badf4ed
konishi <konishi5282@gmail.com>
2016-12-10 21:32:00 +0900

    add table

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <!DOCTYPE html>
+
 <head>
+<title>Dictionary App</title>
+
 </head>
+
 <body>
-Hello World!!!
+<h1>Dictionary App</h1>
+<table>
+  <tr>
+    <th>fluit</th>
+    <th>fluit2</th>
+  </tr>
+  <tr>
+    <td>apple</td>
+    <td>pinapple</td>
+  </tr>
+</table>
+
+
 </body>
 
-</html>
